6efdf36a2ad2949418ebec3e59546fbc0ffe5b68
Nidelson Bonilla <nidelsonbonilla@gmail.com>
2023-05-18 08:51:36 -0600

    primer avance

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed:true },
+  { text: 'Tomar el Curso de Intro', completed:false },
+  { text: 'Llorar con la llorona', completed:false },
+  { text: 'Comer', completed:false },
+];
+
+function App() {
+  return (
+    <>
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem
+           key={todo.text}
+           text={todo.text}
+           completed={todo.completed}/>
+        ))}
+      </TodoList>
+
+
+      <CreateTodoButton />
+      </>
+  );
+}
+
+
+export default App;
